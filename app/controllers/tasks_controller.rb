2a03c69d9792afa0c1a22dfdc87daf5f2628fe33
EC2 Default User <ec2-user@ip-172-31-3-2.us-east-2.compute.internal>
2018-01-12 12:25:56 +0000

    save→update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,7 @@ class TasksController < ApplicationController
   end
   
   def update
-    if @task.save
+    if @task.update
       flash[:success] = 'Taskが正常に更新されました'
       redirect_to @task
     else
